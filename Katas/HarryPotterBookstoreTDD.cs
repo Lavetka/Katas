aa6e1f88e40af27ee8f2d9fc0c0e076253e7952e
lavetos <Hleb_Lavetka@epam.com>
2023-12-20 21:20:20 +0300

    Implement calculating book sum without any discounts

---
+++ Katas/HarryPotterBookstoreTDD.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Katas
+{
+    public class HarryPotterBookstoreTDD
+    {
+        private static readonly double BookPrice = 8.0;
+
+        public static double CalculateTotalPrice(Dictionary<int, int> basket)
+        {
+            double totalPrice = 0;
+
+            while (basket.Values.Sum() > 0)
+            {
+                for (int i = 1; i <= 5; i++)
+                {
+                    if (basket.ContainsKey(i) && basket[i] > 0)
+                    {
+                        totalPrice +=  BookPrice;
+                        basket[i]--;
+                    }
+                }
+            }
+            return totalPrice;
+        }
+    }
+}
+
---
+++ Tests/HarryPotterBookstoreTDDTests.cs
@@ -0,0 +1,23 @@
+﻿using Katas;
+
+namespace Tests
+{
+	public class HarryPotterBookstoreTDDTests
+	{
+        private static IEnumerable<TestCaseData> TestDataWithoutDiscounts()
+        {
+            yield return new TestCaseData(new Dictionary<int, int> { { 1, 1 } }, 8.0);
+            yield return new TestCaseData(new Dictionary<int, int> { { 5, 1 } }, 8.0);
+            yield return new TestCaseData(new Dictionary<int, int> { { 3, 2 } }, 16.0);
+            yield return new TestCaseData(new Dictionary<int, int> { { 5, 5 } }, 40.0);
+        }
+
+        [TestCaseSource(nameof(TestDataWithoutDiscounts))]
+        public void CalculateTotalPrice_withoutDiscount(Dictionary<int, int> basket, double expectedTotalPrice)
+        {
+            double totalPrice = HarryPotterBookstoreTDD.CalculateTotalPrice(basket);
+            Assert.AreEqual(expectedTotalPrice, totalPrice);
+        }
+    }
+}
+
